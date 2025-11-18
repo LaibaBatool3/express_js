6d113f3d49b3fa3c865d73cb003b0617122c1775
Laiba Batool <laibabatool320@gmail.com>
2025-11-18 21:50:33 +0500

    first commit

--- routers/users.js
+++ routers/users.js
@@ -1,38 +1,50 @@
-const express=require('express')
+const express = require("express")
 const router = express.Router()
 
 router.use(logger)
 
-router.get("/", (req,res)=>{
-    res.send("user List")
+router.get("/", (req, res) => {
+  console.log(req.query.name)
+  res.send("User List")
 })
 
-router.get("/new", (req,res)=>{
-    res.send("Users New Form")
-    
+router.get("/new", (req, res) => {
+  res.render("users/new")
 })
 
-router.post('/', (req,res)=>{
-    res.send(`Create User`)
-    })
+router.post("/", (req, res) => {
+  const isValid = false
+  if (isValid) {
+    users.push({ firstName: req.body.firstName })
+    res.redirect(`/users/${users.length - 1}`)
+  } else {
+    console.log("Error")
+    res.render("users/new", { firstName: req.body.firstName })
+  }
+})
 
 router
-    .route("/:id")
-    .get('/:id', (req,res)=>{
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user)
+    res.send(`Get User With ID ${req.params.id}`)
+  })
+  .put((req, res) => {
+    res.send(`Update User With ID ${req.params.id}`)
+  })
+  .delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`)
+  })
 
-    req.params.id
-    res.send(`Get user with ID ${req.params.id}`)
-    })
-    .put("/:id", (req,res)=>{
-    res.send(`Update user with ID ${req.params.of}`)
-    })
-    .delete('/:id', (req,res)=>{
-    req.params.id
-    res.send(`Delete user with ID ${req.params.id}`)
-    })
-const users=[{name:"ALI"}, {name:"Maha"}]
-router.param("id", (req,res,next,id)=>{
-        req.user=users[id]
-        next()
-    })
+const users = [{ name: "Kyle" }, { name: "Sally" }]
+router.param("id", (req, res, next, id) => {
+  req.user = users[id]
+  next()
+})
+
+function logger(req, res, next) {
+  console.log(req.originalUrl)
+  next()
+}
+
 module.exports = router
--- server.js
+++ server.js
@@ -1,22 +1,14 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 
-app.set('view engine', 'ejs')
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.get("/",logger, (req,res)=>{
-    console.log("Here")
-    res.render('index', {text: "World"})
-})
+app.set("view engine", "ejs")
 
-app.use(logger)
-const userRouter = require('./routers/users')
+const userRouter = require("./routes/users")
 
 app.use("/users", userRouter)
 
-function logger(req, res, next){
-         console.log(req.originalUrl)
-    }
-
 app.listen(3000)
-
-
